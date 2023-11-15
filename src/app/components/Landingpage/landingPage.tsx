f932cc486b8904c52407bbda73435cef46fa2a2e
yasaswinviable <111553752+yasaswinviable@users.noreply.github.com>
2023-11-15 20:03:37 +1100

    Landing page table updates
    Added two new tables to the landing page

--- src/app/components/Landingpage/landingPage.tsx
+++ src/app/components/Landingpage/landingPage.tsx
@@ -11,6 +11,66 @@ export default function LandingPage() {
 //         </main> 
 <main className="container w-screen">
 
+{/* <div class="container">
+  <div class="Col1R1"><p>Get quotes from Crypto Lawyers (coming soon) </p></div>
+  <div class="Col1R2"><p>Receive regulatory alerts (coming soon) Set alerts for places you operate in or your legal structure, tokens or token activities,Receive regulatory alerts (coming soon) Set alerts for places you operate in or your legal structure, tokens or token activities,Receive regulatory alerts (coming soon) Set alerts for places you operate in or your legal structure, tokens or token activities,Receive regulatory alerts (coming soon) Set alerts for places you operate in or your legal structure, tokens or token activities,Receive regulatory alerts (coming soon) Set alerts for places you operate in or your legal structure, tokens or token activities,Receive regulatory alerts (coming soon) Set alerts for places you operate in or your legal structure, tokens or token activities,</p></div>
+  <div class="Col1R3"><p>Row 3</p></div>
+  <div class="Col2"><p>Column 1</p></div>
+</div> */}
+
+
+<div class="container">
+  <div class="Empty"></div>
+  <div class="C1BOX1"><p>24/7 Chat</p></div>
+  <div class="C2BOX0"><p>Starter US$600</p></div>
+  <div class="C2Box1"><p>Up to 20 Prompts/ Month</p></div>
+  <div class="C234BOX1"><p><center>Purchase Additional Prompts in bundles of 10 for US$15 each bundle</center></p></div>
+  <div class="C234BOX2"><p><center>Purchase access to the LLM fine-tuned by crupto lawyers for US$5xx</center></p></div>
+  <div class="C3BOX0">Global US$1,200 / year*</div>
+  <div class="C3BOX1">Up to 40 prompts / Month</div>
+  <div class="C4BOX0">Global US 5,500 / year*</div>
+  <div class="C4BOX1">Up to 60 prompts / Month</div>
+  <div class="C5BOX0">Self hosted data set and LLM</div>
+  <div class="C5Box1">✅</div>
+  <div class="C1BOX2"></div>
+  <div class="C2BOX2"></div>
+  <div class="C4BOX2"></div>
+  <div class="C5BOX2"></div>
+  <div class="C3BOX2"></div>
+  <div class="C1BOX3"></div>
+  <div class="C2BOX3"></div>
+  <div class="C3BOX3"></div>
+  <div class="C4BOX3"></div>
+  <div class="C5BOX3"></div>
+  <div class="C1BOX4"></div>
+  <div class="C2BOX4"></div>
+  <div class="C3BOX4"></div>
+  <div class="C4BOX4"></div>
+  <div class="C5BOX4"></div>
+  <div class="C234BOX3"></div>
+  <div class="C1BOX5"></div>
+  <div class="C2BOX5"></div>
+  <div class="C3BOX5"></div>
+  <div class="C4BOX5"></div>
+  <div class="C5BOX5"></div>
+  <div class="C234BOX4"></div>
+  <div class="C1BOX6"></div>
+  <div class="C234BOX5"></div>
+  <div class="C5BOX6"></div>
+  <div class="C1BOX7"></div>
+  <div class="C2BOX6"></div>
+  <div class="C3BOX6"></div>
+  <div class="C4BOX6"></div>
+  <div class="C5BOX7"></div>
+  <div class="C234BOX6"></div>
+  <div class="C1BOX8"></div>
+  <div class="C2BOX7"></div>
+  <div class="C3BOX7"></div>
+  <div class="C4BOX7"></div>
+  <div class="C5BOX8"></div>
+  <div class="C234BOX7"></div>
+</div>
+
 </main>
 
     //     <div className="grid grid-cols-2 gap-1">
